55f9a9fe08c927094546e62cfcb9950f384f1959
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2016-11-21 00:45:02 +0200

    implement class arrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,90 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    final int maxSize = 10000;
+    Resume[] storage = new Resume[maxSize];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
+
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+
+        if (isFull()) {
+            System.out.println("Storage resume crowded");
+        } else if (isExists(storage, r)) {
+            return;
+        }
+        storage[size] = r;
+        size++;
     }
 
-    Resume get(String uuid) {
-        return null;
+    public Resume get(String uuid) {
+        if(exists(uuid)) {
+            for (int i = 0; i < size; i++) {
+                return storage[i];
+            }
+        }
+            return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (exists(uuid)) {
+                storage[i] = storage[size - 1];
+                size--;
+                return;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            if(storage[i] != null)
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return size;
+    }
+
+    private boolean isFull() {
+        if (size == maxSize) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isExists(Resume[] re, Resume r) {
+        for (int i = 0; i < size; i++) {
+            if (re[i].getUuid().equals(r.getUuid())) {
+                System.out.println("This resume is already exists");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean exists(String uuidFound) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuidFound)) {
+                return true;
+            }
+        }
+        System.out.print("Resume don't find  ");
+        return false;
     }
 }
