0f9d9d892b06a3d349d3c2b1f05d33bd906dade2
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-02-21 15:54:20 +0200

    Creat RunWith

--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -1,7 +1,21 @@
 package com.urise.webapp.storage;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
 /**
  * Created by viktoriyasidenko on 2/19/17.
  */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                ArrayStorageTest.class,
+                SortedArrayStorageTest.class,
+                ListStorageTest.class,
+                MapStorageTest.class,
+                MapResumeStorageTest.class
+        }
+)
 public class AllStorageTest {
 }
