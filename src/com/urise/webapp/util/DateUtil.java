7320e503eaa0cb557e04e4c009029e9763545296
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-03-01 11:30:58 +0200

    Creat class DateUtil

--- src/com/urise/webapp/util/DateUtil.java
+++ src/com/urise/webapp/util/DateUtil.java
@@ -1,7 +1,14 @@
 package com.urise.webapp.util;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  * Created by viktoriyasidenko on 2/28/17.
  */
 public class DateUtil {
+
+    public static LocalDate of(int year, Month month){
+        return LocalDate.of(year, month, 1);
+    }
 }
