184ddc41191a56be9d29d4c6f07f0ae92ea8d559
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-03-01 13:40:57 +0200

    Working with file, creat class MainFile

---
+++ src/com/urise/webapp/MainFile.java
@@ -0,0 +1,33 @@
+package com.urise.webapp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by viktoriyasidenko on 3/1/17.
+ */
+public class MainFile {
+    public static void main(String[] args) {
+        File filePath = new File("./.gitignore");
+        try {
+            System.out.println(filePath.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+        File dir = new File("./src/com/urise/webapp");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : list) {
+                System.out.println(name);
+            }
+        }
+        try(FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
