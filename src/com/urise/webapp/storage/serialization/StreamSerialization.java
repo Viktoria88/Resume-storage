56307a1eeef8f121414745f1203a494c19773a54
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-03-05 19:30:04 +0200

    General realisation for FileStorage and PathStorage

--- src/com/urise/webapp/storage/serialization/StreamSerialization.java
+++ src/com/urise/webapp/storage/serialization/StreamSerialization.java
@@ -1,7 +1,17 @@
 package com.urise.webapp.storage.serialization;
 
+import com.urise.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * Created by viktoriyasidenko on 3/5/17.
  */
 public interface StreamSerialization {
+
+    void doWrite(Resume r, OutputStream os) throws IOException;
+
+    Resume doRead(InputStream is) throws IOException;
 }
