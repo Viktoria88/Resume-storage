95d771ba06ba8a12131a3cfd2ef5b0cb655065ee
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-03-05 16:43:39 +0200

    Create constructor for exeption without parametrs: uuid and exeption

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -10,6 +10,10 @@ public class StorageException extends RuntimeException {
 
     private final String uuid;
 
+    public StorageException(String message) {
+        this(message, null, null);
+    }
+
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
