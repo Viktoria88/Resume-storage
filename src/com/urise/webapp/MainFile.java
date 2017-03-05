4e4f0db37f4747a7d4661a7f05f7f159bc29f5e1
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-03-05 15:21:35 +0200

    Print directiry

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -31,21 +31,21 @@ public class MainFile {
         }
 
         System.out.println("printDirectoryRecursion");
-        printDirectoryRecursion(dir);
+        printDirectoryRecursion(dir, "");
 
     }
 
-    public static void printDirectoryRecursion(File dir) {
+    public static void printDirectoryRecursion(File dir, String offset) {
 
         File[] files = dir.listFiles();
 
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("     File: " + file.getName());
+                    System.out.println(offset + "F: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectoryRecursion(file);
+                    System.out.println(offset + "D: " + file.getName());
+                    printDirectoryRecursion(file, offset + " ");
                 }
             }
         }
