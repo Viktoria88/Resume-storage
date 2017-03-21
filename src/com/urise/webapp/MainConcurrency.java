fec9242af018adca9fd951c528b140dbebe8dc12
Viktoriya Sidenko <viktoriyasidenko@MacBook-Air-Viktoriya.local>
2017-03-21 19:47:29 +0200

    Implemented class MainConcurrency

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,7 +1,15 @@
 package com.urise.webapp;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Created by viktoriyasidenko on 3/16/17.
@@ -9,9 +17,22 @@ import java.util.List;
 public class MainConcurrency {
 
     private static int counter;
-    private static final Object LOCK = new Object();
-    private static final Object DEAD_LOCK = new Object();
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+//    private static final Object LOCK = new Object();
+//    private static final Object DEAD_LOCK = new Object();
     private static final int THREADS_NUMBER = 10000;
+//    private static final Lock lock = new ReentrantLock();
+
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
 
 
     public static void main(String[] args) throws InterruptedException {
@@ -44,71 +65,125 @@ public class MainConcurrency {
         System.out.println("Многопоточность, синхронизация");
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
 
         for (int i = 0; i < THREADS_NUMBER; i++){
+            Future<Integer> future = executorService.submit(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                    System.out.println(threadLocal.get().format(new Date()));
+                }
+                latch.countDown();
+                return 5;
+            });
+//            completionService.poll();
+ /*
             Thread thread = new Thread(() -> {
             for(int j = 0; j < 100; j++){
                 mainConcurrency.inc();
             }
+            latch.countDown();
             });
             thread.start();
-            threads.add(thread);
+//            threads.add(thread);
 //            thread.join();
+*/
         }
 
 //        Thread.sleep(500);
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                    e.printStackTrace();
-            }
-        });
-        System.out.println(counter);
+//        threads.forEach(t -> {
+//            try {
+//                t.join();
+//            } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//            }
+//        });
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(mainConcurrency.counter);
+        System.out.println(mainConcurrency.atomicCounter.get());
 
         new MainConcurrency().inc();
 
-        Thread t1 = new Thread(){
-            @Override
-            public void run(){
-                synchronized (LOCK){
-                    Thread.yield();
-                    synchronized (DEAD_LOCK){
-                        System.out.println("Success!");
-                    }
+//        Thread t1 = new Thread(){
+//            @Override
+//            public void run(){
+//                synchronized (LOCK){
+//                    Thread.yield();
+//                    synchronized (DEAD_LOCK){
+//                        System.out.println("Success!");
+//                    }
+//                }
+//            }
+//        };
+//
+//        Thread t2 = new Thread(){
+//            @Override
+//            public void run(){
+//                synchronized (DEAD_LOCK){
+//                    Thread.yield();
+//                    synchronized (LOCK){
+//                        System.out.println("Success!");
+//                    }
+//                }
+//            }
+//        };
+//
+//        t1.start();
+//        t2.start();
+
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+    }
+
+    private static void deadLock(Object lock1, Object lock2){
+        new Thread(()->{
+            System.out.println("Waiting " + lock1);
+            synchronized (lock1){
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " + lock2);
+                synchronized (lock2){
+                    System.out.println("Holding " + lock2);
                 }
             }
-        };
-
-        Thread t2 = new Thread(){
-            @Override
-            public void run(){
-                synchronized (DEAD_LOCK){
-                    Thread.yield();
-                    synchronized (LOCK){
-                        System.out.println("Success!");
-                    }
-                }
-            }
-        };
-
-        t1.start();
-        t2.start();
+        }).start();
     }
 
 //    private static synchronized void inc(){
 //        counter++;
 //    }
 
-    private synchronized void inc() {
+    private /*synchronized*/ void inc() {
 //        synchronized (MainConcurrency.class){
 //        synchronized (this){
 //        synchronized (LOCK){
 //            System.out.println(this);
-            counter++;
-//            wait();
+//        wait();
 //            notifyAll();
+//    }
+
+//        lock.lock();
+//        try {
+//            counter++;
+//        } finally {
+//            lock.unlock();
+//        }
+
+
+        atomicCounter.incrementAndGet();
+
 //        }
     }
 }
